fc3a7188ed62b1e22ad817f03238f6f0af1159b9
fodhil-ben <benhibafodhil@gmail.com>
2024-07-12 09:47:41 +0100

    edited vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        define: {
+          'process.env': {
+            ...process.env,
+            ...env,
+          },
+        },
+        server: {
+          host: true,
+          port: env.VITE_REVERB_PORT || 3000,
+        }
+      };
+
 });
